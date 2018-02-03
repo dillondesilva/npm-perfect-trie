f9056dba5eb5b77a2aa801fefe37c3f5d5ff1a22
Dillon de Silva <dillon.desilva@geeksdiary.com>
2018-02-03 22:12:34 +1100

    Bug fixes and more tests

--- test/index.js
+++ test/index.js
@@ -10,7 +10,57 @@ describe('Tests', () => {
 
   it('should find the node with a certain prefix', () => {
     root.addWord('ten');
-    var node = root.find("te");
-    console.log(node);
+    var node = root.find('te');
+    assert.equal('te', node._prefix);
+    assert.equal(false, node._complete);
   });
+
+  it('should find the node for a complete match', () => {
+    root.addWord('ten');
+    var node = root.find('ten');
+    assert.equal('ten', node._prefix);
+    assert.equal(true, node._complete);
+  });
+
+  it('should not find unmatching words', () => {
+    root.addWord('ten');
+    var node = root.find('ben');
+    assert.equal(null, node);
+  });
+
+  it('getChildren method should work', () => {
+    root.addWord('tell');
+    var node = root.find('tel');
+    var children = node.getChildren();
+    assert.equal('tell', children[0]._prefix);
+  })
+
+  it('getPrefix method should work', () => {
+    root.addWord('tea');
+    var node = root.find('te');
+    var nodePrefix = node.getPrefix();
+    assert.equal('te', nodePrefix);
+  })
+
+  it('Testing finding a node with a non-existing prefix', () => {
+    root.addWord('ten');
+    var node = root.find('telephone');
+    assert.equal(null, node);
+  })
+
+  it('should return back a list of words', () => {
+    var wordsToAdd = ['ten', 'ted', 'tea'];
+    for (var word in wordsToAdd) {
+      root.addWord(wordsToAdd[word]);
+    }
+
+    var wordsList = root.words();
+    var checker = ['ten', 'ted', 'tea', 'tell'].sort();
+    assert.deepEqual(checker, wordsList);
+  })
+
+  it('should return true for checking if a prefix is a word', () => {
+    var node = root.find('tea');
+    assert.equal(true, node.isWord());
+  })
 });
--- trie.js
+++ trie.js
@@ -12,15 +12,11 @@ class Node {
 
   getChildren() {
     // This method will return all the child node objects if any exist
-    if (this._childnodes.length === 0) {
-      return [];
-    } 
-
     var childnodes = [];
-    
-    for(i=0; i < this._childnodes.length; i++) {
-      childnodes.push(this._childnodes[i]);
+    for(var key in this._childnodes) {
+      childnodes.push(this._childnodes[key]);
     }
+
     return childnodes;
   }
 
@@ -40,7 +36,7 @@ class Node {
       var letterToAdd = word[i];
       prefix += letterToAdd;
       if (node._childnodes[letterToAdd]) {
-        node = level[letterToAdd];
+        node = node._childnodes[letterToAdd];
       } else {
         var newNode = new Node(prefix);
         node._childnodes[letterToAdd] = newNode;
@@ -51,7 +47,6 @@ class Node {
     node._complete = true;
   }
 
-  // This is not the function that you are looking for
   find(prefix) {
     var matchFound = false;
     var node = this;
@@ -80,28 +75,26 @@ class Node {
 
     function getWords(node) {
       if (node._complete) {
-        completeWords.push(node.prefix);
+        completeWords.push(node._prefix);
       } 
 
-      if (node._childnodes.length !== 0) {
-        for (i=0;i < node._childnodes.length;i++) {
-          getWords(node._childnodes[i]);
-        }
+      for (var key in node._childnodes) {
+        getWords(node._childnodes[key]);
       }
     }
 
     if (this._complete) {
-      completeWords.push(this.prefix);
+      completeWords.push(this._prefix);
     }
 
-    if (this._childnodes === 0) {
+    if (Object.keys(this._childnodes).length === 0) {
       return completeWords;
     }
 
-    for (i=0;i < this._childnodes.length; i++) {
-      getWords(this._childnodes[i])
+    for (var key in this._childnodes) {
+      getWords(this._childnodes[key])
     }
-
+    
     return completeWords.sort();
   }
 }
